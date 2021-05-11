256dcdfe16acd17e017cf1d39fe9eb27383d82d4
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-05-11 18:02:43 -0300

    Simplificação do tabs

--- projeto_ionic/src/pages/tabs/tabs.ts
+++ projeto_ionic/src/pages/tabs/tabs.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
 
@@ -9,19 +8,7 @@ import { IonicPage, NavController } from 'ionic-angular';
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  tab1Root: any = "ListMasterPage";
-  tab2Root: any = "SearchPage";
-  tab3Root: any = "HistoricoPage";
 
-  tab1Title = " ";
-  tab2Title = " ";
-  tab3Title = " ";
-
-  constructor(public navCtrl: NavController, public translateService: TranslateService) {
-    translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
-      this.tab1Title = values['TAB1_TITLE'];
-      this.tab2Title = values['TAB2_TITLE'];
-      this.tab3Title = values['TAB3_TITLE'];
-    });
+  constructor(public navCtrl: NavController) {
   }
 }
