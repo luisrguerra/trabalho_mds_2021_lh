9e803274cf828998ce986a3521d3c333fd01cc2f
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-05-11 16:40:13 -0300

    Update items.ts

--- projeto_ionic/src/mocks/providers/items.ts
+++ projeto_ionic/src/mocks/providers/items.ts
@@ -8,7 +8,7 @@ export class Items {
 
   defaultItem: any = {
     "name": "Burt Bear",
-    "profilePic": "assets/img/speakers/bear.jpg",
+    //"profilePic": "assets/img/speakers/bear.jpg",
     "about": "Burt is a Bear.",
     "codigo": "xxxxxx",
     "horario": "xxxxxx\nzzzzzzz",
