c0c75446363c6a3c13e51c65dbf5a70da713e870
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-05-17 19:23:47 -0300

    correção dos botões disponiveis no menu de detalhe da disciplina cadastrada

--- projeto_ionic/src/pages/item-detail/item-detail.ts
+++ projeto_ionic/src/pages/item-detail/item-detail.ts
@@ -14,7 +14,18 @@ export class ItemDetailPage {
   constructor(public navCtrl: NavController, navParams: NavParams,public items: Items) {
     this.item = navParams.get('item') || items.defaultItem;
   }
-
+  
+  ionViewDidLoad() {
+    if (this.item.status == "Pré-selecionado"){
+         document.getElementById("doisBotoes").style.display = "inherit";
+         document.getElementById("botaoLongoRetirar").style.display = "none";
+    }
+    else{
+         document.getElementById("doisBotoes").style.display = "none";
+         document.getElementById("botaoLongoRetirar").style.display = "inherit";
+    }
+    
+  }
 
   retirarBotao() {
     this.items.delete(this.item);
