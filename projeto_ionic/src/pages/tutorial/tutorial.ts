d4faa24a5b098f3a640543a340f8bc058c5c09a3
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-05-21 16:18:56 -0300

    melhorias no tutorial

--- projeto_ionic/src/pages/tutorial/tutorial.ts
+++ projeto_ionic/src/pages/tutorial/tutorial.ts
@@ -38,17 +38,27 @@ export class TutorialPage {
           },
           {
             title: "Tela de matrícula",
-            description: values.TUTORIAL_SLIDE2_DESCRIPTION,
+            description: "Aqui é mostrado as disciplinas em processo de matrícula ou já matriculadas.",
             image: 'assets/img/tutorial/tutorial1.png',
           },
           {
+            title: "Disciplinas selecionadas",
+            description: "Ao selecionar um item da tela de matrícula, você visualizará informações sobre as suas solicitações e poderá confirma ou cancelar a sua matrícula.",
+            image: 'assets/img/tutorial/tutorial5.png',
+          },
+          {
             title: "Tela de lista de oferta",
-            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            description: "Na lista de oferta você encontrará as disciplinas disponíveis para matrícula.",
             image: 'assets/img/tutorial/tutorial2.png',
           },
           {
+            title: "Disciplina ofertada",
+            description: "Ao selecionar uma disciplina na lista de oferta, você visualizará informações sobre ela e poderá se matricular.",
+            image: 'assets/img/tutorial/tutorial4.png',
+          },
+          {
             title: "Tela de histórico de matrícula",
-            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            description: "No histórico você visualizará as etapas dos seu processo de matrícula junto da data e hora de cada solicitação.",
             image: 'assets/img/tutorial/tutorial3.png',
           }
         ];
